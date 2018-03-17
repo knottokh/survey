2f276fd61afcd6f1de73289c0967663489c9af3a
Knotto <one2treego@hotmail.com>
2018-03-17 13:34:01 +0000

    with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+  get 'pages/index'
+
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with "rake raroutes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
